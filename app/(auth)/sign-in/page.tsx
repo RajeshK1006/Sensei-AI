4511f0b3f523f05785ff693b400d66cdf523b73a
RajeshK1006 <rajesh.kofficialmail@gmail.com>
2025-05-01 12:45:59 +0530

    homepage addded

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import AuthLayout from "@/components/AuthLayout";
+
+const Page = () => {
+    return (
+        <AuthLayout type="sign-in" />
+    )
+}
+export default Page
